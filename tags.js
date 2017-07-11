1d10329e17d4a5d774caa7e8b9e4fe06a5f29daa
Vladimir Oberemok <nihtalak@gmail.com>
2017-07-11 16:51:05 +0300

    Small improvements

--- tags.js
+++ tags.js
@@ -5,11 +5,27 @@ function filterTag(tag) {
 Vue.component("tags", {
   template: "#tags-template",
   props: {
+    maxTags: {
+      type: Number,
+      default: -1
+    },
+    maxTagLen: {
+      type: Number,
+      default: -1
+    },
     values: {
       type: Array,
       default: function() {
         return [];
       }
+    }
+  },
+  computed: {
+    value: function() {
+      return this.values.join(",");
+    },
+    showInput: function() {
+      return this.maxTags === -1 || this.maxTags > this.values.length;
     }
   },
   data: function() {
@@ -20,11 +36,18 @@ Vue.component("tags", {
       this.values.splice(this.values.indexOf(key), 1);
     },
     add: function() {
+      if (this.maxTags.length <= this.values.length) {
+        return;
+      }
       var tag = filterTag(this.newTag);
       if (tag && tag.length) {
         this.values.push(tag);
       }
       this.newTag = "";
+    },
+    addPrevent: function(e) {
+      e.preventDefault();
+      this.add();
     },
     removeLast: function() {
       if (!this.newTag) {
